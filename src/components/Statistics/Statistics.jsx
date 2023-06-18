0206acef040fb6e75cb3f450598098f3724a6479
Dimekhin Maksym <dimehin-makc@i.ua>
2023-06-18 20:36:02 +0100

    second task
    we add h2 only if we have title prop

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const Statistics = ({title, stats}) =>{
     return(
         <section className="statistics">
-            <h2 className="title">{title}</h2>
+            {title && (<h2 className="title">{title}</h2>)}
             <ul className="stat-list">
                 {stats.map(stat =>(
                     <li className="item" key={stat.id}>
